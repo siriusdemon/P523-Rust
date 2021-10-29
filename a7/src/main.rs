7714c84cb21d0f811f5c98926f509bebd0e1b7eb
siriusdemon <mory2016@126.com>
2021-10-29 18:16:23 +0800

    find a bug in a6

--- a7/src/main.rs
+++ a7/src/main.rs
@@ -11,11 +11,18 @@ use compiler::compile;
 
 fn main() -> std::io::Result<()> {
     let s = "
-    (letrec ([f$0 (lambda () (locals () 80))])
-      (locals (a.1 b.2)
+    (letrec ([f$0 (lambda (x.1) (locals () (+ 1 x.1)))]
+             [g$1 (lambda (x.1) (locals () (- x.1 1)))]
+             [t$2 (lambda (x.1) (locals () (- x.1 1)))]
+             [j$3 (lambda (x.1) (locals () (- x.1 1)))]
+             [i$4 (lambda (x.1) (locals () (- x.1 1)))]
+             [h$5 (lambda (x.1) (locals () (- x.1 1)))])
+      (locals (x.1 a.2 b.3 c.4)
         (begin
-          (set! a.1 (f$0))
-          (set! b.2 (f$0))
-          (* a.1 b.2))))";
+          (set! x.1 80)
+          (set! a.2 (f$0 x.1))
+          (set! b.3 (g$1 x.1))
+          (set! c.4 (h$5 (i$4 (j$3 (t$2 x.1)))))
+          (* a.2 (* b.3 (+ c.4 0))))))";
     compile(s, "t.s")
 }
