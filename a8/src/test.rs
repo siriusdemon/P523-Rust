356db27645ee9aff063f10a42e1061d3577c3639
siriusdemon <mory2016@126.com>
2021-11-09 18:26:37 +0800

    updat

--- a8/src/test.rs
+++ a8/src/test.rs
@@ -40,3 +40,70 @@ fn compile1() {
                 (begin (set! a.1 8) a.1)))))";
     test_helper(s, "c1.s", "3\n");
 }
+
+#[test]
+fn compile2() {
+    let s = "
+    (letrec ([member$0 (lambda (x.1 ls.2)
+                         (locals (size.4 ls.3)
+                           (begin
+                             (set! size.4 (mref ls.2 0))
+                             (if (> x.1 size.4)
+                                 0
+                                 (if (= x.1 (mref ls.2 16))
+                                     1
+                                     (begin
+                                       (set! ls.3 (alloc (* 8 (- size.4 1))))
+                                       (mset! ls.3 0 (- size.4 1))
+                                       (mset! ls.3 8 (+ ls.2 16))
+                                       (member$0 x.1 ls.3)))))))])
+      (locals (ls.1)
+        (begin
+          (set! ls.1 (alloc 48))
+          (mset! ls.1 0 5)
+          (mset! ls.1 8 9)
+          (mset! ls.1 16 2)
+          (mset! ls.1 24 7)
+          (mset! ls.1 32 8)
+          (mset! ls.1 40 3)
+          (member$0 4 ls.1))))";
+    test_helper(s, "c2.s", "0\n");
+}
+
+#[test]
+fn compile3() {
+    let s = "
+    (letrec ([a$1 (lambda (m.5 x.1 y.2)
+                    (locals ()
+                      (begin
+                        (mset! m.5 0 (+ x.1 y.2))
+                        m.5)))])
+      (locals (x.3)
+        (begin
+          (set! x.3 (a$1 (alloc 8) 10 6))
+          (mref x.3 0))))";
+    test_helper(s, "c3.s", "16\n");
+}
+
+#[test]
+fn compile4() {
+    let s = "  
+    (letrec ([a$1 (lambda (m.1 a.2)
+                    (locals ()
+                      (begin
+                        (mset! m.1 a.2 (+ (mref m.1 (- a.2 8))
+                                          (mref m.1 (- a.2 8))))
+                        1)))])
+      (locals (m.3)
+        (begin
+          (set! m.3 (alloc 56))
+          (mset! m.3 0 1)
+          (mset! m.3 8 1)
+          (a$1 m.3 16)
+          (a$1 m.3 24)
+          (a$1 m.3 32)
+          (a$1 m.3 40)
+          (a$1 m.3 48)
+          (mref m.3 48))))";
+    test_helper(s, "c4.s", "\n");
+}
